cdd534fe258cbb18f14ece490331176191e103d5
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-26 19:53:10 -0400

    allowed for dynamic navbars

--- home/src/app/navbar/navbar.component.ts
+++ home/src/app/navbar/navbar.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+    navigationTabs = [
+        {materialIcon: "account_circle", tabName: "Profile"},
+        {materialIcon: "restaurant", tabName: "Meeting"},
+        {materialIcon: "lightbulb", tabName: "Prompt"},
+        {materialIcon: "info", tabName: "Info"}
+    ];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
