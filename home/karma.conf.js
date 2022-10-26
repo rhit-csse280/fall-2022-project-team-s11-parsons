cf3fb57c2f3f1bbd147bee417bfe14ce72ec44b1
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-26 19:31:34 -0400

    trying to start anew with Angular differently

--- home/karma.conf.js
+++ home/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/meal-meetings'),
+      dir: require('path').join(__dirname, './coverage/home'),
       subdir: '.',
       reporters: [
         { type: 'html' },
