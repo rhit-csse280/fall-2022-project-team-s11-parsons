c1a2b97f2d77bd96e66c4c95d3f1509328d540a7
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-26 20:51:15 -0400

    added meeting component

--- home/src/app/meeting-setup/meeting-setup.component.ts
+++ home/src/app/meeting-setup/meeting-setup.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-meeting-setup',
-  templateUrl: './meeting-setup.component.html',
-  styleUrls: ['./meeting-setup.component.css']
+    selector: 'app-meeting-setup',
+    templateUrl: './meeting-setup.component.html',
+    styleUrls: ['./meeting-setup.component.css']
 })
 export class MeetingSetupComponent implements OnInit {
+    @Input() colorlist : readonly string[] = [];
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
