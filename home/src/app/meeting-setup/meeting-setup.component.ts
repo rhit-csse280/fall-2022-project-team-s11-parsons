487825bc3518f4e5fbf7d8bfb2d16f72d05131a7
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-27 04:31:28 -0400

    organized INVITATIONRECEIVED stage of meeting comp

--- home/src/app/meeting-setup/meeting-setup.component.ts
+++ home/src/app/meeting-setup/meeting-setup.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./meeting-setup.component.css']
 })
 export class MeetingSetupComponent implements OnInit {
-    state : string = "WAITING";
+    state : string = "INVITATIONRECEIVED";
     constructor() { }
 
     ngOnInit(): void {
