50d50c5d3a6390d64592b333898d384ae2ed1e90
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-27 00:50:30 -0400

    Adding additional complexity to meeting component.

--- home/src/app/meeting-setup/meeting-setup.component.ts
+++ home/src/app/meeting-setup/meeting-setup.component.ts
@@ -6,6 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['./meeting-setup.component.css']
 })
 export class MeetingSetupComponent implements OnInit {
+    state : string = "INVITATIONRECEIVED";
     constructor() { }
 
     ngOnInit(): void {
