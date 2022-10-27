e088f5746e183f249e16e8676c166833869cfbbb
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-26 21:49:16 -0400

    https://www.youtube.com/watch?v=AAu8bjj6-UI routes

--- home/src/app/app-routing.module.ts
+++ home/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InfoComponent } from './info/info.component';
+import { MeetingSetupComponent } from './meeting-setup/meeting-setup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PromptComponent } from './prompt/prompt.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'profile', component: ProfileComponent },
+    { path: 'meeting', component: MeetingSetupComponent },
+    { path: 'prompt', component: PromptComponent },
+    { path: 'info', component: InfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
