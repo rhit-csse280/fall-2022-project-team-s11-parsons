94a7ad3d5b9216e973f47e4bfb763ab14b32fa89
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-26 22:49:52 -0400

    added a home page

--- home/src/app/app.component.ts
+++ home/src/app/app.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title : string = 'Meal Meetings';
+    username : string = '';
+    constructor() {
+
+    }
     getColor (colorName : string) : string {
         return sessionStorage.getItem(colorName) || "black";
     }
