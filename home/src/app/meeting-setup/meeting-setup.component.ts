656898de2236030b9e0837ed18a802d9806b1ad4
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-28 00:21:54 -0400

    managed coversion of time from MM:HH to integers

--- home/src/app/meeting-setup/meeting-setup.component.ts
+++ home/src/app/meeting-setup/meeting-setup.component.ts
@@ -8,6 +8,9 @@ import { Component, Input, OnInit } from '@angular/core';
 export class MeetingSetupComponent implements OnInit {
     state : string = "SETUP";
     pm: boolean = false;
+    hour: number = 0;
+    minute: number = 0;
+
     constructor() { }
 
     ngOnInit(): void {
@@ -19,6 +22,40 @@ export class MeetingSetupComponent implements OnInit {
 
     setAmPm(pmTrue : boolean) : void {
         this.pm = pmTrue;
+    }
+
+    // A helper function for converting a string to a non-negative integer.
+    // If the integer representation of the string is >= low and <= high,
+    // Then the output should be the value of the integer.
+    // Otherwise, return -1.
+    convertStringToInteger(expression : string, low : number, high : number) : number {
+        for (let i = low; i <= high; i++) {
+            let iString = "" + i;
+            while (iString.length < expression.length) {
+                iString = "0" + iString;
+            }
+            if (iString === expression) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    convertToDetails(timeExpression : string) : void {
+        if (timeExpression.length != 5) {
+            console.log("Wrong Length for MM:HH format");
+            return;
+        } else {
+            const newHour : string = timeExpression.substring(0, 2);
+            const newMinute : string = timeExpression.substring(3, 5);
+            const newHourConverted : number = this.convertStringToInteger(newHour, 1, 12);
+            const newMinuteConverted : number = this.convertStringToInteger(newMinute, 0, 59);
+            if (newHourConverted != -1 && newMinuteConverted != -1) {
+                this.hour = newHourConverted;
+                this.minute = newMinuteConverted;
+                console.log(`${this.hour} ${this.minute}`);
+            }
+        }
     }
 
 }
