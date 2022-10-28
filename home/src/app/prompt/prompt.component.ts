16af7c94115c14b019c898742a68226e8293b8ff
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-27 23:56:47 -0400

    added random selection of prompts

--- home/src/app/prompt/prompt.component.ts
+++ home/src/app/prompt/prompt.component.ts
@@ -7,12 +7,25 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PromptComponent implements OnInit {
     @Input() colorlist : readonly string[] = [];
-    constructor() { }
+    index: number;
+    prompts: readonly string[] = [];
+    constructor() { 
+        this.index = 0;
+        this.prompts = ["What is your name?",
+        "What do you think about this app?",
+        "What do you think about the design of this app?",
+        "Can you think of more prompts to go here?",
+        "Is this the last prompt?"];
+    }
 
     ngOnInit(): void {
     }
 
     getColor (colorName : string) : string {
         return sessionStorage.getItem(colorName) || "black";
+    }
+
+    getNewPrompt() {
+        this.index = Math.floor(Math.random() * this.prompts.length);
     }
 }
