a9e1e5a5eca8ee4dbf6157dadb2e1675a9643147
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-10-28 13:26:10 -0400

    fixed problems

---
+++ home/src/app/meeting-setup/meeting-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meeting-setup',
+  templateUrl: './meeting-setup.component.html',
+  styleUrls: ['./meeting-setup.component.css']
+})
+export class MeetingSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
