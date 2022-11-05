3ffc74b508751757e3c1dfa639e1eb6363ac361d
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-05 19:20:42 -0400

    Revert "After some research, maybe authentication will work?"
    This reverts commit 02424ab3c77f50b2462f4318f82646bc1f9a72c2.

--- home/src/app/signin/signin.component.ts
+++ home/src/app/signin/signin.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import firebase from 'firebase/compat/app';
 import 'rosefire';
 
 @Component({
@@ -9,20 +7,9 @@ import 'rosefire';
     styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
-    /*
-    The setup code for this component was non-trivial. Some sources I used were:
-    https://github.com/angular/angularfire/ and subpages
-    https://ada.csse.rose-hulman.edu/rosefire/javascript-sdk and subpages
-    https://github.com/angular/angularfire/issues/2409#issuecomment-615993136
-
-    Other sources I saw were:
-    https://stackoverflow.com/questions/51656933/angular-6-and-firebase-angularfireauth
-    https://github.com/angular/angularfire/issues/2409#issuecomment-615993136
-
-    */
-    myAfAuth : AngularFireAuth;
-    constructor(public afAuth : AngularFireAuth) {
-        this.myAfAuth = afAuth;
+    afAuth : AngularFireAuth;
+    constructor(afAuth : AngularFireAuth) {
+        this.afAuth = afAuth;
     }
 
     ngOnInit(): void {
@@ -66,7 +53,7 @@ export class SigninComponent implements OnInit {
                 // Use the token to authenticate with your server
                 // checkout the server SDKs for more information.
                 console.log(rfUser);
-                this.myAfAuth.signInWithCustomToken(rfUser.token);
+                this.afAuth.auth().signInWithCustomToken(rfUser.token);
             }
         });
     }
