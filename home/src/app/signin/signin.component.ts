da1ddd8426996c7f88e5668289f0562fa5b63e89
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-05 23:33:44 -0400

    connected sign in and sign up buttons to Rosefire

--- home/src/app/signin/signin.component.ts
+++ home/src/app/signin/signin.component.ts
@@ -18,45 +18,18 @@ export class SigninComponent implements OnInit {
         return sessionStorage.getItem(colorName) || "black";
     }
 
-    sendSignInData(emailValue : string, passwordValue : string) {
-        console.log(emailValue);
-        console.log(passwordValue);
-
+    rosefireSignIn() {
         Rosefire.signIn('fdcea3b8-924f-48eb-8602-9c9355591911', (error, rfUser: RosefireUser) => {
             if (error) {
-              // User not logged in!
-              console.error(error);
-              return;
+                // User not logged in!
+                console.error(error);
+                return;
             } else {
-              // Use the token to authenticate with your server
-              // checkout the server SDKs for more information.
-              console.log(rfUser);
+                // Use the token to authenticate with your server
+                // checkout the server SDKs for more information.
+                console.log(rfUser);
             }
-          });
-          
+        });
 
-        /*
-        let currentAccounts : string | null = sessionStorage.getItem("person");
-        if (!currentAccounts) {
-            currentAccounts = "";
-        }
-        const accounts : string[] = currentAccounts.split("\n");
-        for (const account of accounts) {
-            if (account === "") {
-                //Ignore blank lines
-                continue;
-            }
-            const accountProperties : string[] = account.split(",");
-            const username = accountProperties[0];
-            const email = accountProperties[1];
-            const password = accountProperties[2];
-            if (email === emailValue && password === passwordValue) {
-                sessionStorage.setItem("username", username);
-                document.location.href = "/info";
-                return 0;
-            }
-        }
-        return -1;
-        */
     }
 }
--- home/src/app/signup/signup.component.ts
+++ home/src/app/signup/signup.component.ts
@@ -31,38 +31,17 @@ export class SignupComponent implements OnInit {
         return possibility;
     }
 
-    sendSignUpData(emailValue: string, passwordValue: string) : number {
-        // Determine whether the email is already in the database.
-        // If not, allow the account to be created.
-        console.log("Sign Up goes here");
-        console.log(`${emailValue} ${passwordValue}`);
-        // The value stored in session storage uses a CSV-like format
-        let currentAccounts : string | null = sessionStorage.getItem("person");
-        if (!currentAccounts) {
-            currentAccounts = "";
-        }
-        // Extract the usernames (to avoid repeats) and 
-        const accounts : string[] = currentAccounts.split("\n");
-        const usernames : string[] = [];
-        for (const account of accounts) {
-            if (account === "") {
-                //Ignore blank lines
-                continue;
+    rosefireSignUp() {
+        Rosefire.signIn('fdcea3b8-924f-48eb-8602-9c9355591911', (error, rfUser: RosefireUser) => {
+            if (error) {
+                // User not logged in!
+                console.error(error);
+                return;
+            } else {
+                // Use the token to authenticate with your server
+                // checkout the server SDKs for more information.
+                console.log(rfUser);
             }
-            const accountProperties : string[] = account.split(",");
-            const username = accountProperties[0];
-            usernames.push(username);
-            const email = accountProperties[1];
-            if (email == emailValue) {
-                return -1;
-            }
-        }
-        //Add a new entry
-        const myUsername = this.generateRandomUsername(usernames, 20);
-        currentAccounts += `${myUsername},${emailValue},${passwordValue},,,,,,\n`;
-        sessionStorage.setItem("person", currentAccounts);
-        sessionStorage.setItem("username", myUsername);
-        document.location.href = "/setup";
-        return 0;
+        });
     }
 }
