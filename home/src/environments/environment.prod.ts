76c5d7ec1a8c36eae82baa9ba70caaf79d304982
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-05 23:56:22 -0400

    added Firebase hosting

--- home/src/environments/environment.prod.ts
+++ home/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'meal-meetings',
+    appId: '1:843796174844:web:cda4c9debff814ffb88479',
+    databaseURL: 'https://meal-meetings-default-rtdb.firebaseio.com',
+    storageBucket: 'meal-meetings.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBkFYcpxTzIBKnYNVcOAYo2iTpvJMWCS2c',
+    authDomain: 'meal-meetings.firebaseapp.com',
+    messagingSenderId: '843796174844',
+  },
   production: true
 };
