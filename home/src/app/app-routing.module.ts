06464b1c5910e52678d43d80947bbcd94d5bc354
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-07 19:43:58 -0500

    moved sign in/up to the home page

--- home/src/app/app-routing.module.ts
+++ home/src/app/app-routing.module.ts
@@ -6,17 +6,13 @@ import { MeetingComponent } from './meeting/meeting.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PromptComponent } from './prompt/prompt.component';
 import { SetupComponent } from './setup/setup.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
     { path: 'profile', component: ProfileComponent },
     { path: 'meeting', component: MeetingComponent },
     { path: 'prompt', component: PromptComponent },
     { path: 'info', component: InfoComponent },
-    { path: 'signin', component: SigninComponent },
     { path: 'setup', component: SetupComponent },
-    { path: 'signup', component: SignupComponent },
     { path: '**', component: HomepageComponent }
 ];
 
--- home/src/app/homepage/homepage.component.ts
+++ home/src/app/homepage/homepage.component.ts
@@ -17,4 +17,20 @@ export class HomepageComponent implements OnInit {
         return sessionStorage.getItem(colorName) || "black";
     }
 
+    rosefireSignIn() {
+        Rosefire.signIn('fdcea3b8-924f-48eb-8602-9c9355591911', (error, rfUser: RosefireUser) => {
+            if (error) {
+                // User not logged in!
+                console.error(error);
+                return;
+            } else {
+                // Use the token to authenticate with your server
+                // checkout the server SDKs for more information.
+                console.log(rfUser);
+                sessionStorage.setItem("username", rfUser.token);
+                window.location.href = `/profile`;
+            }
+        });
+
+    }
 }
