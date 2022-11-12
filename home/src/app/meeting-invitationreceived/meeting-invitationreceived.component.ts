df6096ed73937b47ec4ee1d71da1d2448b76789c
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-12 15:00:47 -0500

    got the display on invitation page to work

--- home/src/app/meeting-invitationreceived/meeting-invitationreceived.component.ts
+++ home/src/app/meeting-invitationreceived/meeting-invitationreceived.component.ts
@@ -11,11 +11,13 @@ export class MeetingInvitationreceivedComponent implements OnInit {
     location : string = "";
     otherUser : string = "";
     constructor() {
+    }
+
+    ngOnInit(): void {
         const meetingInfo = sessionStorage.getItem("meetingdata");
         const otherUserNumber = 3 - Number(sessionStorage.getItem("usernumber"));
         if (meetingInfo && this.meetingSource && otherUserNumber) {
             let meetingInfoObject = JSON.parse(meetingInfo);
-            console.log(meetingInfoObject);
             const hour = parseInt(String(meetingInfoObject["hour" + this.meetingSource]));
             const minute = parseInt(String(meetingInfoObject["minute" + this.meetingSource]));
             const pm = (String(meetingInfoObject["pm" + this.meetingSource]) == "true") ? "PM" : "AM";
@@ -23,9 +25,6 @@ export class MeetingInvitationreceivedComponent implements OnInit {
             this.location = String(meetingInfoObject["location" + this.meetingSource]);
             this.otherUser = String(meetingInfoObject["user" + otherUserNumber]);
         }
-    }
-
-    ngOnInit(): void {
     }
 
     getColor(colorName: string): string {
