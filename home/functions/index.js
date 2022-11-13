53f19aa22192a0c4cf69e44ab9b59662d34aeef4
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-13 09:29:37 -0500

    beginning work on my function

--- home/functions/index.js
+++ home/functions/index.js
@@ -17,3 +17,25 @@ exports.callMe = functions.https.onCall((data, context) => {
     return 57;
 });
 */
+
+const fs = require("fs");
+let database; // db.json stores the entire database of meeting requests
+fs.readFile(__dirname + "/db.json", (err, data) => {
+    database = JSON.parse(data.toString());
+})
+
+exports.formAMeeting = functions.firestore.document("/UsersWaitingForMeal/{userid}")
+.onCreate((snap, context) => {
+    // Get the data
+    const myData = snap.data();
+    let minutesAfterMidnight = (myData["hour"] % 12) * 60 + myData["minute"];
+    if (myData["pm"]) {
+        minutesAfterMidnight += 60 * 12;
+    }
+    // Because users cannot change their preferences once they submit
+    // We know that any compatible matches must be with this new user.
+    database.push({"totalMinutes" : minutesAfterMidnight, "location" : myData["location"], "username" : myData["username"], "userid" : myData["userid"]});
+    
+    // This should write back to the file.
+    fs.writeFile(__dirname + "/db.json", JSON.stringify(myData));
+});
