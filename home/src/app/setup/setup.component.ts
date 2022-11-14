98502a870eae3676aa6672c1cc50c742d7cf2a5d
Joseph Clark Parsons <43769180+rhit-parsonjc@users.noreply.github.com>
2022-11-13 22:01:45 -0500

    ensured that users need to click on Terms of Service to continue

--- home/src/app/setup/setup.component.ts
+++ home/src/app/setup/setup.component.ts
@@ -10,10 +10,26 @@ export class SetupComponent implements OnInit {
     constructor() { }
 
     ngOnInit(): void {
+        // Hide the navbar every second
+        // This doesn't do much really other than verify for certain that it is hidden.
+        setInterval(() => {
+            const navbar = document.getElementById("backgroundDiv");
+            if (navbar) {
+                navbar.style.display = "hidden";
+            }
+        }, 1000);
     }
 
     getColor(colorName: string): string {
         return sessionStorage.getItem(colorName) || "black";
     }
 
+    deleteAccountAndRedirect() {
+        const button = document.getElementById("uselessButtonDeleteAccount");
+        if (button) {
+            button.click();
+            alert("I'm sorry, but you have to accept the Terms of Service to have a Meal Meetings account. If you have any objections to the Terms of Service, email josephcpar@gmail.com.");
+            window.location.href = "/";
+        }
+    }
 }
